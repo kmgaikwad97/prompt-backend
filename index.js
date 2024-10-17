4663f26ad0c26986528b0f9c72339f37dae946fe
kmgaikwad97 <kmgaikwad97@gmail.com>
2024-10-17 20:58:29 +0530

    changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDb = require("./src/config/database"); // Moved to the top for clarity
-
+console.log("hello world")
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
